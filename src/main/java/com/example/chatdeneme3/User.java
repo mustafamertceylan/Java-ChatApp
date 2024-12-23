fd9045d89b8cbeae206cdb1807fe3442bc56adbd
mustafamertceylan <155013058+mustafamertceylan@users.noreply.github.com>
2024-12-23 18:01:34 +0300

    user nesnesinin nesne bağlantısı sağlan giriş ekranında

---
+++ src/main/java/com/example/chatdeneme3/User.java
@@ -0,0 +1,41 @@
+package com.example.chatdeneme3;
+import java.util.Date;
+public class User extends Chat{
+    public String nickName;
+    private String UserName;
+    private String UserLastName;
+    private String UserPassword;
+    User(String UserName,String UserLastname,String UserPassword,String nickName){
+        super("1234"+UserName,new Date());
+        this.nickName=nickName;
+        this.UserName=UserName;
+        this.UserLastName=UserLastname;
+        this.UserPassword=UserPassword;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        User.this.UserName = userName;
+    }
+
+    public String getUserLastName() {
+        return UserLastName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        UserLastName = userLastName;
+    }
+
+    public String getUserPassword() {
+        return UserPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        UserPassword = userPassword;
+    }
+
+
+}
